60cd9f834267f89013c67feef51b8bc1ce5ce864
peteGSX <peteracole@outlook.com.au>
2024-03-06 18:50:37 +1000

    Win64 and copy config bug fix

--- ex_installer/version.py
+++ ex_installer/version.py
@@ -17,6 +17,7 @@ Version history:
             - Add link to DCC-EX News articles about EX-Installer to the Info menu
             - Ensure the config backup popup is always launched within the app window geometry
             - Add a save log button to device monitor to save the device logs to a text file
+            - Fix bug where copying existing config files for EX-Turntable and EX-IOExpander causes an exception
 0.0.17      - Move fonts to a separate common class
             - Change default font to Arial for Windows/Mac and FreeSans for Linux
             - Update various modules versions to resolve Dependapot identified vulnerabilities
