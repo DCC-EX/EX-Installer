f34ca7b4d3e6a5973bbf0287916ef7ed343a4c81
Peter Akers <akersp62@gmail.com>
2024-06-11 11:35:24 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
-autodoc_mock_imports = ['customkinter']
+autodoc_mock_imports = ['customtkinter']
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
